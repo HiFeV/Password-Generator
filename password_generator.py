da9adfee2ed9b1f937bea97f783c3164716cfd77
HiFeV <67747114+HiFeV@users.noreply.github.com>
2021-04-11 17:30:07 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,6 +3,7 @@
 
 import string
 import random
+import time
 
 length = int(input("Choose the length of the password: "))
 
@@ -15,3 +16,24 @@ passwd = random.sample(elements, length)
 
 result = "".join(passwd)
 print("[+] Your password is: ", result)
+
+ask = input("Do you want to save your password in a textfile ? YES / NO: ")
+
+
+if ask == "NO":
+	print("Exiting...")
+	time.sleep(3)
+	exit()
+
+elif ask == "YES":
+	reminder = open("generated_password.txt", "w")
+	text_file = reminder.write(result)
+	reminder.close()
+	print("[+] generated_password.txt successfully saved")
+	print("Exiting...")
+	time.sleep(3)
+	exit()
+
+else:
+	print("You can only answer YES or NO")
+	print(ask)
